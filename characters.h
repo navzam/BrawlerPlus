d8337cacc64abf513468276186c5fd22c3bc84d7
Braden McDorman <bmcdorman@gmail.com>
2010-03-14 17:10:58 -0500

    Cleaned up development reference in characters.h

--- characters.h
+++ characters.h
@@ -18,11 +18,6 @@ public:
     static Characters& getInstance(void);
 };
 
-/* Mario = 0, DonkeyKong, Link, Samus, Kirby, Fox, Pikachu, Marth, MrGameAndWatch, Luigi,
-		 DiddyKong, Zelda, Pit, MetaKnight, Falco, PokemonTrainer, Ike, Snake, Peach, Yoshi,
-		 Ganandorf, IceClimbers, KingDeDeDe, Lucario, Ness, Sonic, Bowser, Wario, ToonLink,
-		 ROB, Olimar, CaptainFalcon, JigglyPuff, Lucas */
-
 struct Mario : public Character { Mario() : Character("Mario") {} };
 struct DonkeyKong : public Character { DonkeyKong() : Character("Donkey Kong") {} };
 struct Link : public Character { Link() : Character("Link") {} };
